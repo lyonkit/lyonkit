7144d7a3d4d9c5a49c1e1716f2c9d4ea74047b67
Léo Coletta <mail.leo.coletta@gmail.com>
2022-06-26 17:52:52 +0200

    fix: Fix tests

--- packages/bloks/tests/components/HelloWorld.test.ts
+++ packages/bloks/tests/components/HelloWorld.test.ts
@@ -6,10 +6,10 @@ describe('HelloWorld.vue', () => {
   it('should render', () => {
     const wrapperH1 = mount(HelloWorld)
     expect(wrapperH1.text()).toContain('Hello World !')
-    expect(wrapperH1.html()).toMatchInlineSnapshot('"<h1 class=\\"text-purple-400\\"> Hello World ! </h1>"')
+    expect(wrapperH1.html()).toMatchInlineSnapshot('"<h1 class=\\"purple\\" data-v-469af010=\\"\\"> Hello World ! </h1>"')
 
     const wrapperH2 = mount(HelloWorld, { props: { size: 4 } })
     expect(wrapperH2.text()).toContain('Hello World !')
-    expect(wrapperH2.html()).toMatchInlineSnapshot('"<h4 class=\\"text-purple-400\\"> Hello World ! </h4>"')
+    expect(wrapperH2.html()).toMatchInlineSnapshot('"<h4 class=\\"purple\\" data-v-469af010=\\"\\"> Hello World ! </h4>"')
   })
 })
